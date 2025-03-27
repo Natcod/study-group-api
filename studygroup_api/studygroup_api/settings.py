c5e607fd891063ecf0a4bce722f7e451595a52aa
Natcod <natnaelshetaye1@gmail.com>
2025-03-27 14:26:28 +0300

    Add Django REST framework and authentication configuration

--- studygroup_api/studygroup_api/settings.py
+++ studygroup_api/studygroup_api/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'api',
+
 ]
 
 MIDDLEWARE = [
@@ -66,6 +70,15 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 WSGI_APPLICATION = 'studygroup_api.wsgi.application'
 
